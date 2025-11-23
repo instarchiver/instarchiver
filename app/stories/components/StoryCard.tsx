d805aca624cc6b0b436b7ccb5cae3f5b7ebfa3b8
Arter Tendean <arter.tendean.07@gmail.com>
2025-11-23 14:13:37 +0700

    Remove story hover effect

--- app/stories/components/StoryCard.tsx
+++ app/stories/components/StoryCard.tsx
@@ -49,7 +49,7 @@ export function StoryCard({ story, onPreview }: StoryCardProps) {
 
       {/* Full-width media section - no padding */}
       <div
-        className="relative w-full border-t-2 border-[var(--border)] group aspect-[9/16] cursor-pointer hover:opacity-90 transition-opacity"
+        className="relative w-full border-t-2 border-[var(--border)] aspect-[9/16] cursor-pointer"
         onClick={() => onPreview?.(story)}
       >
         <Image
@@ -60,18 +60,6 @@ export function StoryCard({ story, onPreview }: StoryCardProps) {
           className="object-cover"
           priority
         />
-        <div className="absolute inset-0 bg-black/0 hover:bg-black/10 transition-colors flex items-center justify-center opacity-0 hover:opacity-100">
-          <div className="bg-white/90 rounded-full p-2 shadow-lg">
-            <svg className="w-6 h-6 text-black" fill="currentColor" viewBox="0 0 20 20">
-              <path d="M8.433 7.418c.155-.103.346-.196.567-.267v1.698a2.305 2.305 0 01-.567-.267C8.07 8.34 8 8.114 8 8c0-.114.07-.34.433-.582zM11 12.849v-1.698c.22.071.412.164.567.267.364.242.433.468.433.582 0 .114-.07.34-.433.582a2.305 2.305 0 01-.567.267z" />
-              <path
-                fillRule="evenodd"
-                d="M10 18a8 8 0 100-16 8 8 0 000 16zM9.049 5.927c.3-.921 1.603-.921 1.902 0a1.286 1.286 0 001.851.63c.423-.305 1.172-.305 1.495 0 .324.305.324 1.054 0 1.36a1.286 1.286 0 00-.63 1.851c-.305.423-.305 1.172 0 1.495.305.324 1.054.324 1.36 0a1.286 1.286 0 001.851.63c.423.305.423 1.172 0 1.495-.305.324-1.054.324-1.36 0a1.286 1.286 0 00-1.851.63c-.305.423-1.172.423-1.495 0-.324-.305-.324-1.054 0-1.36a1.286 1.286 0 00.63-1.851c-.423-.305-.423-1.172 0-1.495.305-.324.305-1.054 0-1.36a1.286 1.286 0 00-1.851-.63c-.305-.423-1.172-.423-1.495 0-.324.305-.324 1.054 0 1.36a1.286 1.286 0 00-.63 1.851c.305.423.305 1.172 0 1.495-.305.324-1.054.324-1.36 0a1.286 1.286 0 00-1.851-.63c-.423-.305-.423-1.172 0-1.495.305-.324 1.054-.324 1.36 0a1.286 1.286 0 001.851-.63z"
-                clipRule="evenodd"
-              />
-            </svg>
-          </div>
-        </div>
       </div>
 
       <CardFooter className="border-t-2 border-[var(--border)] bg-[var(--secondary-background)] flex-col gap-2 p-3">
