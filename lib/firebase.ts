76692a50513ad26f55200643b11f38e8f4e4b55b
Arter Tendean <arter.tendean.07@gmail.com>
2025-11-25 22:53:11 +0700

    Update firebase

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,6 +1,5 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
-import { getAnalytics } from 'firebase/analytics';
 import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -14,13 +13,11 @@ const firebaseConfig = {
   storageBucket: 'instarchiver.firebasestorage.app',
   messagingSenderId: '162052642058',
   appId: '1:162052642058:web:3ce745f9502a943a6416f3',
-  measurementId: 'G-PQJH839F0H',
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 const auth = getAuth(app);
 const provider = new GoogleAuthProvider();
 
-export { app, analytics, auth, provider, signInWithPopup };
+export { app, auth, provider, signInWithPopup };
