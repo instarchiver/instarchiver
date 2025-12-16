2e49276deb72afb0eb0ea0a0bb2ba138a125105b
Arter Tendean <arter.tendean.07@gmail.com>
2025-12-16 23:10:52 +0700

    Update

--- app/types/instagram/post.ts
+++ app/types/instagram/post.ts
@@ -2,6 +2,8 @@
  * Instagram Post related types
  */
 
+import type { InstagramUser } from './user';
+
 export interface PostMedia {
   id: number;
   thumbnail_url: string;
@@ -11,27 +13,6 @@ export interface PostMedia {
   media: string | null;
   created_at: string;
   updated_at: string;
-}
-
-export interface InstagramUser {
-  uuid: string;
-  has_stories: boolean;
-  has_history: boolean;
-  instagram_id: string;
-  username: string;
-  full_name: string;
-  profile_picture: string;
-  biography: string;
-  is_private: boolean;
-  is_verified: boolean;
-  media_count: number;
-  follower_count: number;
-  following_count: number;
-  allow_auto_update_stories: boolean;
-  allow_auto_update_profile: boolean;
-  created_at: string;
-  updated_at: string;
-  api_updated_at: string;
 }
 
 export type PostVariant = 'normal' | 'video' | 'carousel';
