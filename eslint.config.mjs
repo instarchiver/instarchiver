451c401156964ce3ee679e1117fba91c95c571d3
Arter Tendean <arter.tendean.07@gmail.com>
2025-12-20 19:00:17 +0700

    Fix github actions error

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,22 +1,14 @@
-import { dirname } from 'path';
-import { fileURLToPath } from 'url';
-import { FlatCompat } from '@eslint/eslintrc';
+import nextCoreWebVitals from "eslint-config-next/core-web-vitals";
+import nextTypescript from "eslint-config-next/typescript";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
 
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [
-  ...compat.extends('next/core-web-vitals', 'next/typescript'),
-  {
-    files: ['**/*.{js,jsx,ts,tsx}'],
-    rules: {
-      '@typescript-eslint/no-unused-vars': 'off',
-    },
+const eslintConfig = [...nextCoreWebVitals, ...nextTypescript, {
+  files: ['**/*.{js,jsx,ts,tsx}'],
+  rules: {
+    '@typescript-eslint/no-unused-vars': 'off',
   },
-];
+}, {
+  ignores: ["node_modules/**", ".next/**", "out/**", "build/**", "next-env.d.ts"]
+}];
 
 export default eslintConfig;
